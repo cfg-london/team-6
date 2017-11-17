d48fbb71093d9855744b7329d77b32b3b4fbddfc
Zicong Ma <zm1216@ic.ac.uk>
2017-11-17 22:26:24 +0000

    David/Zicong more tryout with sql

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ connection = sqlite3.connect("test.db")
 
 sql_command = """
   CREATE TABLE IF NOT EXISTS laureate (
-    first_name VARCHAR(10),
-    last_name VARCHAR(10)
+    first_name VARCHAR(30),
+    last_name VARCHAR(30)
   );
 """
 cursor = connection.cursor()
@@ -15,5 +15,9 @@ sql_command = """INSERT INTO laureate (first_name, last_name)
 
 cursor.execute(sql_command)
 
-cursor.execute('SELECT *')
-cursor.close()
+cursor.execute('SELECT * from laureate')
+
+print (cursor.fetchall())
+
+connection.commit()
+connection.close()
