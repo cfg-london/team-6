a9db72be5d6b7b6b897aa4d2f94b08cbe68822d9
Jack Pordi <pp2916@ic.ac.uk>
2017-11-17 23:52:51 +0000

    Prize pool initialization done

--- back_end/caller.py
+++ back_end/caller.py
@@ -1,9 +1,44 @@
 import requests
+import json
+from prize import Prize, calc_prize_id
 
 class Caller(object):
 
     def __init__(self):
         self.url = "http://api.nobelprize.org/v1/"
 
-    def get_laureates(self):
-        laureate_list = requests.get()
+    def get_all_laureates(self):
+        self.laureate_list = requests.get(self.url + "laureate.json?").json()['laureates']
+        return self.laureate_list
+
+    def get_all_prizes(self):
+        self.prize_list = requests.get(self.url + "prize.json?").json()['prizes']
+        return self.prize_list
+
+    def initialize_laureate_pool(self):
+        self.laureate_pool = {}
+        #for laureate_data in self.
+
+
+    def initialize_prize_pool(self):
+         self.prize_pool = {}
+         for prize_data in self.get_all_prizes():
+             prize_key = calc_prize_id(prize_data)
+             self.prize_pool[prize_key] = Prize(prize_data)
+        
+
+    def show_all_prizes(self):
+        for id, p in self.prize_pool.items():
+            p.show()
+
+
+api = Caller()
+api.initialize_prize_pool()
+print(json.dumps(api.get_all_laureates(), indent=4, sort_keys=True))
+
+
+ 
+ 
+ 
+ 
+
--- back_end/prize.py
+++ back_end/prize.py
@@ -1,24 +1,39 @@
 import json
 import requests
-#import Category as Category
 from enum import Enum
 
-class Category(Enum):
-    PHYSICS   = 10000
-    CHEMISTRY = 20000
-    MEDICINE  = 30000
-    PEACE     = 40000
-    ECONOMICS = 50000
-
 class Prize(object):
-    def __init__(self, year, category, laureate_list):
-        self.id =  category.value + year
-        self.year =  year
-        self.category = category
-        self.laureate_list = laureate_list
 
-prize = Prize(1998, Category.PHYSICS, [1,2])
-print(str(prize.id))
+    def __init__(self, data):
+        self.id =  calc_prize_id(data)
+        self.year =  int(data['year'])
+        self.category = data['category']
+        self.gen_laureates(data['laureates'])
+
+    def gen_laureates(self, laureates):
+        self.laureate_list = []
+        for l in laureates:
+            self.laureate_list.append(int(l['id']))
+
+    def show(self):
+        laureates = ""
+        for i in self.laureate_list:
+            laureates += str(i) + " "
+        print(self.category + " prize in " + str(self.year) + " awarded to laureates: " + laureates)
+
+
+def calc_prize_id(data):
+    return_id = int(data['year'])
+    switcher = {
+            'physics': 10000,
+            'chemistry': 20000,
+            'medicine' : 30000,
+            'peace'    : 40000,
+            'literature': 50000,
+            'economics': 60000,
+            }
+    return return_id + switcher[data['category']]
+
 
 
 
