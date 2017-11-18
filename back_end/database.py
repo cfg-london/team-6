5cffe4a100747a806ec007d9cc1042e22c21957c
Zicong Ma <zm1216@ic.ac.uk>
2017-11-18 02:16:56 +0000

    caller and database creation linked

--- back_end/database.py
+++ back_end/database.py
@@ -5,18 +5,27 @@ from caller import Caller
 connection = sqlite3.connect("test.db")
 
 cursor = connection.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS laureate (id int, poo bit, image_link VARCHAR(30), wiki_link VARCHAR(30), prize_list VARCHAR(30), first_name VARCHAR(30), last_name VARCHAR(30), dob DATE, dod DATE, gender int, organisation VARCHAR(30));")
+cursor.execute("""CREATE TABLE IF NOT EXISTS laureate (
+  id int,
+  poo bit,
+  image_link VARCHAR(30), 
+  wiki_link VARCHAR(30), 
+  prize_list VARCHAR(30),
+  firstname VARCHAR(30), 
+  surname VARCHAR(30), 
+  dob DATE, 
+  dod DATE, 
+  gender int,
+  born_city VARCHAR(30),
+  born_country VARCHAR(30), 
+  organisation VARCHAR(30));""")
 
 api = Caller()
 api.initialize_prize_pool()
 
 for id, laureate in api.laureate_pool.items(): 
-  str_list = ''.join(str(e) for e in laureate.prize_list)
-  cursor.execute("INSERT INTO laureate VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (laureate.id, 1, laureate.image_link, laureate.wiki_link, str_list, laureate.firstname, laureate.surname, laureate.dob, laureate.dod, laureate.gender, None))
+  cursor.execute("INSERT INTO laureate VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (laureate.id, 1, laureate.image_link, laureate.wiki_link, str(laureate.prize_list), laureate.firstname, laureate.surname, laureate.dob, laureate.dod, laureate.gender, None))
 
-name = "pordi"
-
-cursor.execute("SELECT * FROM laureate")
 
 print (cursor.fetchall())
 connection.commit()
